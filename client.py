3b14e48ec0ff7505b437bf49a6904806af77cde8
arsondog <80226983+arsondog@users.noreply.github.com>
2022-04-17 16:51:52 -0400

    Update V0.1
    .gitignore has been updated.
    
    client.py has been updated
    
    clientexe.py has been added.
    
    theme.json has been added.

--- client.py
+++ client.py
@@ -3,7 +3,7 @@
 try:
     import ___________
     print("File has been imported")
-    print("File Type:")
+    print("File Type: ")
     print("File Name:")
     print("Folder Name:")
     print("---------")
@@ -18,3 +18,18 @@ except ImportError:
 """ File is supposed to return "Error: File failed to import" because the import file is a placeholder and doesn't 
 exist. """
 # --------------------------------------------------------------- #
+try:
+    import clientexe
+    print("File has been imported")
+    print("File Type: Python File (.py)")
+    print("File Name: clientexe")
+    print("Folder Name: Main Module")
+    print("---------")
+
+
+except ImportError:
+    print("Error: File failed to import")
+    print("File Type: Python File (.py) ")
+    print("File Name: clientexe")
+    print("Folder Name: Main Module ")
+    print("---------")
---
+++ clientexe.py
@@ -0,0 +1,4 @@
+import pygame_gui
+
+manager = pygame_gui.UIManager((800, 600), 'theme.json')
+

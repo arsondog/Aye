25a2a748c686f5a47c0e912700edcdf7486b7d0c
arsondog <80226983+arsondog@users.noreply.github.com>
2022-04-17 15:57:45 -0400

    Client has been created and .gitignore has been created.

---
+++ client.py
@@ -0,0 +1,20 @@
+# --------------------------------------------------------------- #
+""" Template for importing files """
+try:
+    import ___________
+    print("File has been imported")
+    print("File Type:")
+    print("File Name:")
+    print("Folder Name:")
+    print("---------")
+
+except ImportError:
+    print("Error: File failed to import")
+    print("File Type:")
+    print("File Name:")
+    print("Folder Name:")
+    print("---------")
+
+""" File is supposed to return "Error: File failed to import" because the import file is a placeholder and doesn't 
+exist. """
+# --------------------------------------------------------------- #

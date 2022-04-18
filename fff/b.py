4715911f9a74082d569e625baefc5e799e786e4f
arsondog <80226983+arsondog@users.noreply.github.com>
2022-04-18 13:21:55 -0400

    0.2.0 Storyline is being created.
    For right now, the story is going to be a text based story. At the moment it's in the python console based.

---
+++ fff/b.py
@@ -0,0 +1,71 @@
+input("Start")
+print("Welcome to the Mythverse.")
+print("A long time ago, 200 years ago, Mythverse was an ordinary universe")
+print("However, 150 years ago, our universe changed. Our universe became separated and people that were once allied and"
+      " at peace turned against each other.")
+print("This war lasted for 50 years, and only one person survived."
+      "This person is known as the Raven of Death."
+      "However, the Raven of Death didn't force his reign upon his people"
+      " in fact he allowed his people to decide what they wanted to do.")
+print("100 years ago, a taskforce with the aim of finding the Raven of Death was created"
+      " with the aim to find out what happened during the 50 years of unrecorded history."
+      " But they haven't been able to find the Raven of Death."
+      " However, our world is in grave danger again, people are starting to fight each other."
+      " The task force's goal hasn't changed.")
+name = input("Hello! What is your name? ")
+print("???: Hey {0}! Wake up!".format(name))
+print("???: Get out of bed! Dad needs your help out on the farm harvesting the food.")
+print("You: Ok. I'm getting out of my bed. Chill out. (Now that I think about it, the dream I just had was weird.)")
+print("You are groggy. You get up and change out of your pajamas, and into farmer clothes.")
+print("You run out of your room and run out of the door but your mom stops you.")
+print("Mom: Wait {0}! I need to give you some food that you and your dad can eat later!".format(name))
+print("You: FOR REAL?! Your cooking is delicious! LET'S GO!!!!!!!")
+print("(You take the food out of your mom's hands and run out of the door! You run into the field and you run to your "
+      "dad.)")
+print("Dad: Is that what I think it is?")
+print("You: Yep! Mom's cooking!")
+print("Dad: Alright. Put it on that rock. (He points to the rock.)")
+print("(You put it on the rock.)")
+print("Dad: Alright! {0}, lets harvest all of the food.".format(name))
+print("Meanwhile, above the planet of Planet1")
+print("???: Is the person we are looking for here on this planet?")
+print("???: Yeah, that's right, the Raven of Death is located on this planet.")
+print("Meanwhile, back on the surface, {0} and Dad have harvested the food after 1 hour. They start put all the food "
+      "in a bag and start eating their food on the rock.".format(name))
+print("Dad: Hey, {0}. I need to talk to you about something.".format(name))
+print("You: Dad, yeah, what's up?")
+print("(Dad at first wanted to say something, but he decides to not say it.)"
+      )
+print("Dad: Never mind.")
+print("You: Ok, if you say so.")
+print("You: Time to dig in!")
+print("Dad: You betcha! :)")
+print("(You and Dad gobble down the amazing food that Mom made for them.)")
+print("(You and Dad finish gobbling down the food.)")
+print("Dad: Hey son, time to go back inside to bring back the food to give to Mom so she can cook the food into "
+      "products to sell tomorrow. I'll do the seed planting by myself so go!")
+print("(You run to the house with the bag of grain.)")
+print("(You arrived home.)")
+print("Mom: {0}, where's your father?".format(name))
+print("You: He doing the seed planting. He said that he wanted me to bring the grain here so you can make the bread "
+      "that we can sell tomorrow.")
+print("Mom: Really? If you say so. (Meanwhile Mom has a bad feeling. I hope he doesn't get captured because he's the "
+      "Raven of Death.)")
+print("Mom: Wait! {0}, go out and help your father.".format(name))
+print("You: If I do, I need to have one of the delicious bread loaves.")
+print("Mom: Sure, but only one loaf.")
+print("You: Fair!")
+print("(You run out of the house and go to the fields, however you find your father laying on the fields, "
+      "full of blood.)")
+print("(You scream in rage and despair.)")
+print("(Your mom runs out to see you on the ground and in despair.)")
+print("Mom: Oh, no! I knew that when he asked to do it alone, something was up.")
+print("(Your mom attempts to use her healing power, but it doesn't work.)")
+print("You: Wait, Mom! Why isn't it working...")
+print("Mom: He's already... dead... (She starts crying.)")
+print("You scream in despair, and start crying excessively.")
+print("You bury your dad and take the knife that you noticed was on his back. You bury your dad so that your sister "
+      "doesn't see the gruesome sight.)")
+print("You hide the knife in your pocket.")
+print("Mom: What are you going to do now?")
+print("You: (You start to cry) I don't... know...")
